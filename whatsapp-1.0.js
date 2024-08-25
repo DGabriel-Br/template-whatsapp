0504a741d2afc6f8bc8dca73f70d7b98accf0391
DGabriel-Br <davidsantos.cmf@gmail.com>
2024-08-25 20:27:53 -0300

    Update whatsapp-1.0.js

--- whatsapp-1.0.js
+++ whatsapp-1.0.js
@@ -53,7 +53,7 @@ const userAvatar = botBody.querySelector('img[elementtiming="Bot avatar"]');
 const status = varStatus;
 
 const audioNot = document.createElement('audio');
-audioNot.src = 'https://igorlemoes.com.br/files/whatsapp/audio_whatsapp.mp3';
+audioNot.src = 'https://cdn.jsdelivr.net/gh/DGabriel-Br/template-whatsapp@main/audio_whatsapp.mp3';
 let mensagesLength = 0;
 
 setInterval(() => {
